342c8a42b0b6a2e45c9d55b6285742f415a43926
yzw <13035846175@163.com>
2019-02-23 13:46:12 +0800

    添加config高可用

--- business-service-order/src/main/java/com/lucky/order/BusinessServiceOrderApplication.java
+++ business-service-order/src/main/java/com/lucky/order/BusinessServiceOrderApplication.java
@@ -2,7 +2,7 @@ package com.lucky.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * order启动类
@@ -11,7 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @date 2019/2/18 23:07
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class BusinessServiceOrderApplication {
 
     public static void main(String[] args) {
--- business-service-order/src/main/java/com/lucky/order/controller/OrderController.java
+++ business-service-order/src/main/java/com/lucky/order/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.lucky.order.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import java.util.Map;
  * @date 2019/2/18 22:56
  */
 @RestController
+@RefreshScope
 public class OrderController {
 
     @Value("${name:testName}")
